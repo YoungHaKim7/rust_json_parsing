d9a0b7c3f42618d3bd058cdb3921f76cb6222e60
YoungHa Kim <ytok1108@aol.com>
2022-07-27 20:32:24 +0900

    test print

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rust_json_parsing
+
+Parsing JSON in Rust using serde and reqwest
+<br>
+Let's Get Rusty
+<br>
+[Parsing JSON in Rust using serde and reqwest](https://youtu.be/ogpE4hviXyA)
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let todos = reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{todos:#?}");
+
+    Ok(())
+}
