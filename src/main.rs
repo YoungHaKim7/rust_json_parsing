2496639f249a1bf989385f45076591f41387e86c
YoungHa Kim <ytok1108@aol.com>
2022-07-27 21:46:16 +0900

    new_todo__result: _Todo _     user_id: 1_     id: Some_         201_     __     title: __Subscribe to Let_s Get Rusty__,     completed: false, ___

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,22 @@ struct Todo {
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let todos: Vec<Todo> = reqwest::Client::new()
-        .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+    let new_todo = Todo {
+        user_id: 1,
+        id: None,
+        title: "Subscribe to Let's Get Rusty".to_owned(),
+        completed: false,
+    };
+
+    let new_todo: Todo = reqwest::Client::new()
+        .post("https://jsonplaceholder.typicode.com/todos")
+        .json(&new_todo)
         .send()
         .await?
         .json()
         .await?;
-    println!("{todos:#?}");
+
+    println!("{new_todo:#?}");
 
     Ok(())
 }
